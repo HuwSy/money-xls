9395e113429ba8d369c4330bd77bf6d3c7402301
Huw <HuwSy@users.noreply.github.com>
2025-12-07 17:23:11 +0000

    Support table or div

--- scripts.js
+++ scripts.js
@@ -1523,7 +1523,9 @@ function Init() {
   Spending = document.getElementById("spending").getElementsByTagName('tr');
   Upcoming = document.getElementById("upcoming").getElementsByTagName('tr');
   Plans = document.getElementById("plans").getElementsByTagName('tr');
-  Formulas = document.getElementById("formulas").getElementsByTagName('tr');
+  Formulas = document.getElementById("formulas").children[0];
+  if (Formulas.tagName == "TBODY")
+    Formulas = Formulas.children[0];
 
   XLSX_CALC.import_functions({
     'FIND': function (a, b, c) {
